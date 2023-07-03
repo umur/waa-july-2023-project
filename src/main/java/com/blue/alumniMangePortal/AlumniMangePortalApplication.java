2e6a98fe91e063ec15fef31aa54d33855c724a04
TIMOTHY BALABA <72457041+Timothy-Balaba@users.noreply.github.com>
2023-07-03 14:13:29 -0500

    initial commit

---
+++ src/main/java/com/blue/alumniMangePortal/AlumniMangePortalApplication.java
@@ -0,0 +1,13 @@
+package com.blue.alumniMangePortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniMangePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniMangePortalApplication.class, args);
+	}
+
+}
