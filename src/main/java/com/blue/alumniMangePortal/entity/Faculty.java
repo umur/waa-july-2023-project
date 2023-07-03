e13b7baf7690d785e939fb01e1fce08e19e695c5
curthbertjeremiah <130601489+curthbertjeremiah@users.noreply.github.com>
2023-07-03 15:41:58 -0500

    first faculty update

--- src/main/java/com/blue/alumniMangePortal/entity/Faculty.java
+++ src/main/java/com/blue/alumniMangePortal/entity/Faculty.java
@@ -1,25 +1,29 @@
 package com.blue.alumniMangePortal.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
-
+@Setter
+@Getter
 @Entity
 public class Faculty {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String fname;
-    private String lname;
+    private String first_name;
+    private String last_name;
     private String password;
     private String email;
-    private String phoneNumber;
+    private String phone_number;
+    @ManyToOne
     private Department department;
     private String title;
+    @OneToMany
     private List<Comment> comments;
+    @OneToOne
     private Address address;
-
+    private boolean is_admin;
+    private boolean is_deleted;
 }
