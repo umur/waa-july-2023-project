19aaa98f2925193b7514d7fb5011f1e70459ce99
joanchifamba <130855791+joanchifamba@users.noreply.github.com>
2023-07-03 17:51:26 -0500

    Add changes

--- src/main/java/waa/miu/AlumniManagementPortal/entity/Comment.java
+++ src/main/java/waa/miu/AlumniManagementPortal/entity/Comment.java
@@ -1,4 +1,17 @@
 package waa.miu.AlumniManagementPortal.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Faculty faculty;
 }
