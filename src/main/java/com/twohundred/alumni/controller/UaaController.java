48d6fdc021cd3307463a5b8c8f73c4b8b2f5273f
dinomov <dinomov@gmail.com>
2023-07-05 15:54:15 -0500

    Alumni management project
    Group "200":
    
    added logging

--- src/main/java/com/twohundred/alumni/controller/UaaController.java
+++ src/main/java/com/twohundred/alumni/controller/UaaController.java
@@ -4,10 +4,12 @@ import com.twohundred.alumni.entity.dto.request.LoginRequest;
 import com.twohundred.alumni.entity.dto.request.RegisterRequest;
 import com.twohundred.alumni.entity.dto.response.LoginResponse;
 import com.twohundred.alumni.service.AuthService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/uaa")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -18,6 +20,7 @@ public class UaaController {
 
     @PostMapping("/signin")
     public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginRequest request) {
+        log.info("Start login process");
         return ResponseEntity.ok(authService.authenticate(request));
     }
 
