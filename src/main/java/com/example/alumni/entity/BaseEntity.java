c4d9382f478b2fcf529451f2e3255bd478ba5c47
Taha Elsayed <tahazayed@gmail.com>
2023-07-06 11:45:21 -0500

    fix mapsid

--- src/main/java/com/example/alumni/entity/BaseEntity.java
+++ src/main/java/com/example/alumni/entity/BaseEntity.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @JsonIgnore
