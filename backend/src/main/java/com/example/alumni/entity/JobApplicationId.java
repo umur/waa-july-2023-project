97386e3a74d4c6018e3370a1894b841f9ce3a7f1
Taha Elsayed <tahazayed@gmail.com>
2023-07-13 20:54:16 -0500

    Use Setter and Getter instead of data to fix null id

--- backend/src/main/java/com/example/alumni/entity/JobApplicationId.java
+++ backend/src/main/java/com/example/alumni/entity/JobApplicationId.java
@@ -1,25 +1,17 @@
 package com.example.alumni.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Embeddable
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Setter
+@Getter
 public class JobApplicationId implements Serializable {
 
-//    @ManyToOne
-//    @JoinColumn(name = "user_id")
-//    private User user;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "job_advertisement_id")
-//    private JobAdvertisement jobAdvertisement;
     private Long userId;
     private Long jobAdvertisementId;
 }
