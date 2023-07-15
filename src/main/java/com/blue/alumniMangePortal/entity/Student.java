94f993af7d8d5bcbf8d555c5a449d2675eb7c0dd
TIMOTHY BALABA <72457041+Timothy-Balaba@users.noreply.github.com>
2023-07-15 14:25:14 -0500

    update On Student

--- src/main/java/com/blue/alumniMangePortal/entity/Student.java
+++ src/main/java/com/blue/alumniMangePortal/entity/Student.java
@@ -31,7 +31,7 @@ public class Student {
     @ManyToOne
     private Major major;
     @OneToMany
-    @JsonManagedReference
+//    @JsonManagedReference
     private List<JobsAdvertise> jobsAdvertisedList=new ArrayList<>();
 //    private CurrentWorkPlace;
     @OneToOne
