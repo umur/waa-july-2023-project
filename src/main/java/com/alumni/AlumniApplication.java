6a5eb87d61e1e054fa1d36f2805acbc295a7669a
KDRAA12 <Kmc4pro@gmail.com>
2023-07-15 14:48:43 -0500

    adding cors filter (#8)

--- src/main/java/com/alumni/AlumniApplication.java
+++ src/main/java/com/alumni/AlumniApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class AlumniApplication {
@@ -16,6 +23,8 @@ public class AlumniApplication {
         String seed="w7A&e5LpUs8FxEv2FaBR";
         return new BCryptPasswordEncoder(10, new SecureRandom(seed.getBytes(StandardCharsets.UTF_8)));
 	}
+
+
 
 
 	public static void main(String[] args) {
--- src/main/java/com/alumni/Service/impls/JobAdvertisementServiceImpl.java
+++ src/main/java/com/alumni/Service/impls/JobAdvertisementServiceImpl.java
@@ -32,7 +32,7 @@ public class JobAdvertisementServiceImpl implements JobAdvertisementService {
     @Override
     public List<JobAdvertisementResponseDto> getList(int page, int size, List<Long> tags, String state, String city) {
 
-List<JobAdvertisement> list= new ArrayList<>();
+List<JobAdvertisement> list;
     if(tags!=null)
         list=repository.findAllByTagsInAndStateContainsIgnoreCaseAndCityContainsIgnoreCaseOrderByPostedAtDesc(tags,state,city,PageRequest.of(page,size));
     else
