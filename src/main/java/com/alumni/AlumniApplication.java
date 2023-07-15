39cbec822d7e7108ff6ffa02e657398672ee9a2e
kdraa12 <kmc4pro@gmail.com>
2023-07-15 14:42:43 -0500

    adding cors filter

--- src/main/java/com/alumni/AlumniApplication.java
+++ src/main/java/com/alumni/AlumniApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class AlumniApplication {
@@ -17,6 +24,8 @@ public class AlumniApplication {
         return new BCryptPasswordEncoder(10, new SecureRandom(seed.getBytes(StandardCharsets.UTF_8)));
 //		return new BCryptPasswordEncoder();
 	}
+
+
 
 
 	public static void main(String[] args) {
