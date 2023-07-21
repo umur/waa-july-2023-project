db3c287522b11f9cedd7a1bcc25b6f4fe319f972
KDRAA12 <Kmc4pro@gmail.com>
2023-07-21 00:42:54 -0500

    adding cors filter (#12)

--- src/main/java/com/alumni/AlumniApplication.java
+++ src/main/java/com/alumni/AlumniApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class AlumniApplication {
@@ -20,9 +23,6 @@ public class AlumniApplication {
         String seed="w7A&e5LpUs8FxEv2FaBR";
         return new BCryptPasswordEncoder(10, new SecureRandom(seed.getBytes(StandardCharsets.UTF_8)));
 	}
-
-
-
 
 
 
